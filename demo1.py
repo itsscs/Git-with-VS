88ce046a6e042bac81a7d05f5c1cb5ca79542056
CS <cs@abc.com>
2025-01-22 10:58:45 +0530

    2nd version of even/odd function

--- demo1.py
+++ demo1.py
@@ -1,9 +1,11 @@
 def find_even_or_odd(number):
-    if number % 2 == 0:
-        return "Even"
+    if number != 0:
+        if number % 2 == 0:
+            return "Even"
+        else:
+            return "Odd"
     else:
-        return "Odd"
-
+        return "Zero"
 # Example usage
 number = int(input("Enter a number: "))
 result = find_even_or_odd(number)
